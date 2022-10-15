19e463750e6d6a82b5c84f7b607b14030039ca62
jakeisadev <starowicz@protonmail.com>
2022-10-15 15:04:32 -0400

    finished mobile view

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,6 +7,7 @@ import coffee2 from "../images/coffee2.png";
 import coffee3 from "../images/coffee3.png";
 import coffee4 from "../images/coffee4.png";
 import Button from "@mui/material/Button";
+import oliver from '../images/oliver1.jpg'
 
 function Info() {
   return (
@@ -41,6 +42,44 @@ function Info() {
           What kinds?
         </Button>
       </div>
+      <div className="review-container pt-5">
+        <p>
+          "Mugs is the best coffee shop in town. Oliver and his employees are so
+          great and the atmosphere is great for meeting your friends, business,
+          book clubs, board gaming and more. Forgo the corporate chains and
+          enjoy this fantastic small business."<br></br>
+          <span>~ Google Reviewer</span>
+        </p>
+      </div>
+      <div className="oliver mt-5 mb-5 d-flex flex-column">
+        <strong>Oliver Wu</strong> <br />
+        <img src={oliver} alt="" className="pb-3"/>
+        <em>Audiophile, Explorer, Business owner</em> <br></br>
+        <p className="pt-3 oliver-text">
+        on the corner of Park Rd
+        and Selwyn Ave, Oliver Wu has been hard at work serving the people of
+        Charlotte for over 11 years. Though, when you love what you do, time
+        never seems to have gone by that quick. As the owner of one of South
+        Charlotte's most renowned cafe's, Oliver is up early at 3am or 4am
+        depending on the day, energized, and ready to prepare "Mug's Coffee" for
+        another day of great coffee, tasty food, and warm memories. A little
+        known fact about Oliver is that "Mug's Coffee" is only one of his many
+        business ventures. For him, running "Mug's Coffee" is purely for fun.
+        Success and incredible service doesn't come easy though. Oliver once
+        stated: "When I started Mugs, I was here all the time working 16 hour
+        days." which speaks to the level of devotion and pride he takes in
+        providing for his customers.
+        </p>
+      </div>
+      <footer className="d-flex justify-content-center align-items-center pt-3">
+        <div className="business-info">
+          <ul className="info">
+            <li>Mugs' Coffee</li>
+            <li>5126 Park Rd #1d, Charlotte, NC 28209, United States</li>
+            <li> Dine-in · Takeaway · No delivery</li>
+          </ul>
+        </div>
+      </footer>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,58 @@ import "../resources/navbar.css";
 
 function Navbar() {
   return (
+    <>
+      <nav role="navigation">
+        <div id="menuToggle">
+          <input type="checkbox" />
+
+          <span></span>
+          <span></span>
+          <span></span>
+
+          <ul id="menu">
+            <a href="#">
+              <li>Home</li>
+            </a>
+            <a href="#">
+              <li>About</li>
+            </a>
+            <a href="#">
+              <li>Info</li>
+            </a>
+            <a href="#">
+              <li>Contact</li>
+            </a>
+            <a href="https://erikterwan.com/" target="_blank">
+              <li>Show me more</li>
+            </a>
+          </ul>
+        </div>
+      </nav>
+      {/* Navbar type split */}
       <nav className="navbar">
         <IconButton size="large" edge="start" color="inherit" aria-label="logo">
           <CoffeeIcon className="coffee-icon" />
         </IconButton>
         <ul className="nav-links mb-0">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Menu</a></li>
-          <li><a href="#">Events</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Menu</a>
+          </li>
+          <li>
+            <a href="#">Events</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
         </ul>
       </nav>
+    </>
   );
 }
 
