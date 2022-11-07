f21420155c306eae9fb15b3588b4bc0a3010f846
jakeisadev <starowicz@protonmail.com>
2022-11-07 05:52:45 -0500

    nav styling and main styling changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,37 @@
 import React from "react";
 import { IconButton } from "@mui/material";
 import CoffeeIcon from "@mui/icons-material/Coffee";
-import "../resources/navbar.css";
+import "../resources/newNavbar.css";
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+  console.log('Coffee Icon:', CoffeeIcon);
   return (
     <>
-      <nav role="navigation">
+    <div className="nav">
+  <input type="checkbox" id="nav-check" />
+  <div className="nav-header">
+    <div className="nav-title">
+    <CoffeeIcon className="coffee-icon" />
+    </div>
+  </div>
+  <div className="nav-btn">
+    <label for="nav-check">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+  </div>
+  
+  <div className="nav-links">
+    <a href="//github.io/jo_geek" target="_blank">Github</a>
+    <a href="http://stackoverflow.com/users/4084003/" target="_blank">Stackoverflow</a>
+    <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">LinkedIn</a>
+    <a href="https://codepen.io/jo_Geek/" target="_blank">Codepen</a>
+    <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">JsFiddle</a>
+  </div>
+</div>
+      {/* <nav role="navigation">
         <div id="menuToggle">
           <input type="checkbox" />
 
@@ -34,7 +58,6 @@ function Navbar() {
             </a>
           </ul>
       </nav>
-      {/* Navbar type split */}
       <nav className="navbar">
         <IconButton size="large" edge="start" color="inherit" aria-label="logo">
           <CoffeeIcon className="coffee-icon" />
@@ -53,7 +76,7 @@ function Navbar() {
             <Link to="./Contact"><a href="/#">Contact</a></Link>
           </li>
         </ul>
-      </nav>
+      </nav> */}
     </>
   );
 }
