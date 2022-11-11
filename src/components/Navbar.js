cc4c55ba8c386f62797f653266980728e316b7a6
jakeisadev <starowicz@protonmail.com>
2022-11-11 13:48:21 -0500

    added sticky and position fixes across home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,81 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { IconButton } from "@mui/material";
 import CoffeeIcon from "@mui/icons-material/Coffee";
 import "../resources/newNavbar.css";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  console.log('Coffee Icon:', CoffeeIcon);
+  
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const hide = () => setIsOpen(false);
+  const show = () => setIsOpen(true);
+
+
   return (
     <>
-    <div className="nav">
-  <input type="checkbox" id="nav-check" />
-  <div className="nav-header">
-    <div className="nav-title">
-    <CoffeeIcon className="coffee-icon" />
-    </div>
-  </div>
-  <div className="nav-btn">
-    <label for="nav-check">
-      <span></span>
-      <span></span>
-      <span></span>
-    </label>
-  </div>
-  
-  <div className="nav-links">
-    <Link to="/"><a href="/#" target="_blank">Home</a></Link>
-    <Link to="./Menu"><a href="/#" target="_blank">Menu</a></Link>
-    <Link to="./Events"><a href="/#" target="_blank">Events</a></Link>
-    <Link to="./Contact"><a href="/#" target="_blank">Contact</a></Link>
-  </div>
-</div>
-      {/* <nav role="navigation">
-        <div id="menuToggle">
-          <input type="checkbox" />
-
-          <span></span>
-          <span></span>
-          <span></span>
-
+      <div className="nav">
+        <input type="checkbox" id="nav-check" />
+        <div className="nav-header">
+          <div className="nav-title">
+            <CoffeeIcon className="coffee-icon" />
+          </div>
         </div>
-        <ul id="menu">
-            <a href="/#">
-              <li>Home</li>
+        <div className="nav-btn">
+          <label for="nav-check">
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+        </div>
+
+        <div className="nav-links">
+          <Link to="/">
+            <a href="/#" target="_blank">
+              Home
             </a>
-            <a href="/#">
-              <li>About</li>
+          </Link>
+          <Link to="./Menu">
+            <a href="/#" target="_blank">
+              Menu
             </a>
-            <a href="/#">
-              <li>Info</li>
+          </Link>
+          <Link to="./Events">
+            <a href="/#" target="_blank">
+              Events
             </a>
-            <a href="/#">
-              <li>Contact</li>
+          </Link>
+          <Link to="./Contact">
+            <a href="/#" target="_blank">
+              Contact
             </a>
-            <a href="/#">
-              <li>Show me more</li>
-            </a>
-          </ul>
-      </nav>
-      <nav className="navbar">
-        <IconButton size="large" edge="start" color="inherit" aria-label="logo">
-          <CoffeeIcon className="coffee-icon" />
-        </IconButton>
-        <ul className="nav-links mb-0">
-          <li>
-            <Link to="/"><a href="/#">Home</a></Link>
-          </li>
-          <li>
-            <Link to="./Menu"><a href="/#">Menu</a></Link>
-          </li>
-          <li>
-          <Link to="./Events"><a href="/#">Events</a></Link>
-          </li>
-          <li>
-            <Link to="./Contact"><a href="/#">Contact</a></Link>
-          </li>
-        </ul>
-      </nav> */}
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
